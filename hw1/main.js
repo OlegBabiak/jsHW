f9a9c56a734b94b7fe20c88d4d484ca5d679c77f
OlegBabiak <ovbabiak@gmail.com>
2023-11-21 20:57:19 +0200

    Commit hw1

--- hw1/main.js
+++ hw1/main.js
@@ -1,1 +1,51 @@
-console.log("hello");
+let hello = "hello";
+let owu = "owu";
+let com = "com";
+let country = "ua";
+let one = 1;
+let ten = 10;
+let minus = -999;
+let nextNum = 123;
+let pi = 3.14;
+let someNum = 2.7;
+let someElseNum = 16;
+let boolTrue = true;
+let boolF = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(country);
+console.log(one);
+console.log(ten);
+console.log(minus);
+console.log(nextNum);
+console.log(pi);
+console.log(someNum);
+console.log(someElseNum);
+console.log(boolTrue);
+console.log(boolF);
+
+
+let firstName = "Oleg";
+let middleName = "Volodymyrovych";
+let lastName = "Babiak";
+
+let fullName = firstName +" "+ middleName+" "+ lastName;
+
+console.log(fullName);
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log("type of a is: " + typeof a);
+console.log("type of b is: " + typeof b);
+console.log("type of c is: " + typeof c);
+
+let yourName = prompt("your name");
+let yourMiddleName = prompt("your middle name");
+let yourAge = parseInt(prompt("your age"));
+
+console.log(yourName);
+console.log(yourMiddleName);
+console.log(yourAge);
